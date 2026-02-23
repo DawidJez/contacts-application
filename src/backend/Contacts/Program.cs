96efdb01fa04c0994ce53b0c1496047b420c8f9c
Lake <jd.jezierskidawid@gmail.com>
2026-02-23 22:36:50 +0100

    Added JWT

--- src/backend/Contacts/Program.cs
+++ src/backend/Contacts/Program.cs
@@ -1,8 +1,16 @@
 using Contacts.Data;
 using Microsoft.EntityFrameworkCore;
 
+// Authorization packages
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Database
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -12,6 +20,35 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
 });
 
+// Authorization
+builder.Services.AddAuthorization();
+
+// Hash password
+builder.Services.AddScoped<PasswordHasher<Contacts.Data.Entities.User>>();
+
+// JWT config
+var jwtSection = builder.Configuration.GetSection("Jwt");
+var jwtKey = jwtSection["Key"] ?? throw new Exception("Missing Jwt:Key in configuration");
+var jwtIssuer = jwtSection["Issuer"] ?? throw new Exception("Missing Jwt:Issuer in configuration");
+var jwtAudience = jwtSection["Audience"] ?? throw new Exception("Missing Jwt:Audience in configuration");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+        ClockSkew = TimeSpan.FromMinutes(1)
+    };
+});
+
+// App start up
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -20,6 +57,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapControllers();
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 app.Run();
