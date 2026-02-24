6839bfa1328f3c9d5cc760fe58c59f76b2e6b8b5
Lake <jd.jezierskidawid@gmail.com>
2026-02-24 23:53:52 +0100

    It looks like I just have finished tbs

---
+++ src/backend/Contacts/Dtos/Contacts/EditContact.cs
@@ -0,0 +1,16 @@
+namespace Contacts.Dtos.Contacts;
+
+public sealed class UpdateContactRequest
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public int? SubcategoryId { get; set; }
+    public string? CustomSubcategory { get; set; }
+}
--- src/backend/Contacts/Endpoints/ContactsEndpoints.cs
+++ src/backend/Contacts/Endpoints/ContactsEndpoints.cs
@@ -18,6 +18,7 @@ public static class ContactsEndpoints
         group.MapGet("/{id:int}", GetContactDetails);
         group.MapPost("", CreateContact).RequireAuthorization();
         group.MapDelete("", DeleteContact).RequireAuthorization();
+        group.MapPut("", EditContact).RequireAuthorization();
 
         return app;
     }
@@ -71,22 +72,28 @@ public static class ContactsEndpoints
         var exists = await db.Contacts.AnyAsync(x => x.Email == email);
         if (exists) return Results.Conflict("Contacts email already exists");
 
+        if (email.Length > 320) return Results.BadRequest("Email max length is 320.");
+
         if (!Regex.IsMatch(email, @"^([^@\s]+@[^@\s]+\.[^@\s]+)$")) // Simple regex -> sth@sth.sth
             return Results.BadRequest("Invalid email format.");
 
         // Password
         if (req.Password.Length < 8) return Results.BadRequest("Password must be at least 8 characters.");
+        if (req.Password.Length > 255) return Results.BadRequest("Password max length is 255 characters.");
 
         // String requires: lower char + upper char + digit + (not letter, not digit) special char
         if (!Regex.IsMatch(req.Password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$")) 
             return Results.BadRequest("Password must have upper and lower cahracter, digit and special character.");
 
         // First name and last name
+        if (string.IsNullOrWhiteSpace(req.FirstName) || string.IsNullOrWhiteSpace(req.LastName))
+            return Results.BadRequest("First name and last name are required.");
+
         var firstname = req.FirstName.Trim();
         var lastname = req.LastName.Trim();
 
-        if (firstname.Length == 0) return Results.BadRequest("First name is required.");
-        if (lastname.Length == 0) return Results.BadRequest("Last name is required.");
+        if (firstname.Length > 100) return Results.BadRequest("First name max length is 100.");
+        if (lastname.Length > 100) return Results.BadRequest("Last name max length is 100.");
 
         // Category
         var category = await db.Categories.FirstOrDefaultAsync(el => el.Id == req.CategoryId);
@@ -100,31 +107,31 @@ public static class ContactsEndpoints
         if (categoryName == "Służbowy")
         {
             if (!string.IsNullOrWhiteSpace(req.CustomSubcategory))
-                return Results.BadRequest("CustomSubcategory must be empty for category 'Służbowy'.");
+                return Results.BadRequest("Custom subcategory must be empty for category 'Służbowy'.");
 
             // If subcategory exists it must belong to the selected category
             var subcExists = await db.Subcategories.AnyAsync(el => el.Id == req.SubcategoryId && el.CategoryId == req.CategoryId);
 
-            if (!subcExists) return Results.BadRequest("Invalid SubcategoryId for selected category.");
+            if (!subcExists) return Results.BadRequest("Invalid subcategory id for selected category.");
         }
         else if (categoryName == "Inny")
         {
             if (string.IsNullOrWhiteSpace(req.CustomSubcategory))
-                return Results.BadRequest("CustomSubcategory is required for category 'Inny'.");
+                return Results.BadRequest("Custom subcategory is required for category 'Inny'.");
 
             if (req.CustomSubcategory.Length > 100)
-                return Results.BadRequest("CustomSubcategory max length is 100.");
+                return Results.BadRequest("Custom subcategory max length is 100.");
 
             if (req.SubcategoryId is not null)
-                return Results.BadRequest("SubcategoryId must be empty for category 'Inny'.");
+                return Results.BadRequest("Subcategory id must be empty for category 'Inny'.");
         }
         else // "Prywatny"
         {
             if (req.SubcategoryId is not null)
-                return Results.BadRequest("SubcategoryId must be empty for this category.");
+                return Results.BadRequest("Subcategory id must be empty for this category.");
 
             if (!string.IsNullOrWhiteSpace(req.CustomSubcategory))
-                return Results.BadRequest("CustomSubcategory must be empty for this category.");
+                return Results.BadRequest("Custom subcategory must be empty for this category.");
         }
 
         // Phone number *optional
@@ -132,7 +139,6 @@ public static class ContactsEndpoints
         {
             req.PhoneNumber = req.PhoneNumber.Trim();
             var phone = req.PhoneNumber;
-            if (phone.Length > 16) return Results.BadRequest("PhoneNumber max length is 16.");
 
             if (!System.Text.RegularExpressions.Regex.IsMatch(phone, @"^(\+?[0-9]{7,15})$")) 
                 return Results.BadRequest("Invalid phone number format. (Make sure to include international prefix)");
@@ -170,5 +176,166 @@ public static class ContactsEndpoints
         return Results.Ok($"Contact {id} has been deleted.");
     }
 
+    private static async Task<IResult> EditContact ( int id, UpdateContactRequest req, AppDbContext db, PasswordHasher<Contact> hasher )
+    {
+        var foundContact = await db.Contacts.FirstOrDefaultAsync(el => el.Id == id);
+        if (foundContact is null) return Results.NotFound();
 
+        // Checks if there is anything to update
+        // Using is not null instead: IsNullOrWhiteSpace, because we allow clearing optional elements
+        var hasAny =
+            req.FirstName is not null ||
+            req.LastName is not null ||
+            req.PhoneNumber is not null ||
+            req.Email is not null ||
+            req.Password is not null ||
+            req.CategoryId is not null ||
+            req.SubcategoryId is not null ||
+            req.CustomSubcategory is not null;
+
+        if (!hasAny) return Results.BadRequest("Nothing to update.");
+
+        // Validating the data to put
+        // First and last name
+        if (req.FirstName is not null)
+        {
+            var name = req.FirstName.Trim();
+            if (name.Length == 0) return Results.BadRequest("FirstName cannot be empty.");
+            if (name.Length > 100) return Results.BadRequest("FirstName max length is 100.");
+            foundContact.FirstName = name;
+        }
+
+        if (req.LastName is not null)
+        {
+            var name = req.LastName.Trim();
+            if (name.Length == 0) return Results.BadRequest("LastName cannot be empty.");
+            if (name.Length > 100) return Results.BadRequest("LastName max length is 100.");
+            foundContact.LastName = name;
+        }
+
+        // Phone number
+        if (req.PhoneNumber is not null)
+        {
+            req.PhoneNumber = req.PhoneNumber.Trim();
+            var phone = req.PhoneNumber;
+
+            if (phone.Length == 0)
+            {
+                // clearing phone
+                foundContact.PhoneNumber = null;
+            }
+            else
+            {
+                if (!System.Text.RegularExpressions.Regex.IsMatch(phone, @"^(\+?[0-9]{7,15})$")) 
+                    return Results.BadRequest("Invalid phone number format. (Make sure to include international prefix)");
+                
+                foundContact.PhoneNumber = phone;
+            }
+        }
+
+        // Email
+        if (req.Email is not null)
+        {
+            var email = req.Email.Trim();
+            if (email.Length == 0) return Results.BadRequest("Email cannot be empty.");
+            if (email.Length > 320) return Results.BadRequest("Email max length is 320.");
+
+            var exists = await db.Contacts.AnyAsync(x => x.Email == email && x.Id != id);
+            if (exists) return Results.Conflict("Contacts email already exists");
+
+            if (!Regex.IsMatch(email, @"^([^@\s]+@[^@\s]+\.[^@\s]+)$")) // Simple regex -> sth@sth.sth
+                return Results.BadRequest("Invalid email format.");
+
+            foundContact.Email = email;
+        }
+
+        // Password -> New password
+        if (!string.IsNullOrWhiteSpace(req.Password))
+        {
+            if (req.Password.Length < 8) return Results.BadRequest("Password must be at least 8 characters.");
+            if (req.Password.Length > 255) return Results.BadRequest("Password max length is 255 characters.");
+
+            // String requires: lower char + upper char + digit + (not letter, not digit) special char
+            if (!Regex.IsMatch(req.Password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$")) 
+
+            return Results.BadRequest("Password must have upper and lower cahracter, digit and special character.");
+            foundContact.Password = hasher.HashPassword(foundContact, req.Password);
+        }
+
+        // Category, Subcategory and CustomSubcategory
+        if (req.CategoryId is not null || req.SubcategoryId is not null || req.CustomSubcategory is not null)
+        {
+            // Use new category id if it came with request and it must exist
+            var newCategoryId = req.CategoryId ?? foundContact.CategoryId;
+            var category = await db.Categories.FirstOrDefaultAsync(el => el.Id == newCategoryId);
+            if (category is null) return Results.BadRequest("Invalid CategoryId.");
+
+            var categoryName = category.Name;
+
+            // Custom category
+            string? custom;
+            if (req.CustomSubcategory is null)
+            {
+                custom = foundContact.CustomSubcategory;
+            }
+            else
+            {
+                // If not null then it may be "" 
+                if (string.IsNullOrWhiteSpace(req.CustomSubcategory))
+                    custom = null; // if so -> clear it
+                else
+                    custom = req.CustomSubcategory.Trim(); // New custom
+            }
+
+            if (custom is not null && custom.Length > 100)
+                return Results.BadRequest("CustomSubcategory max length is 100.");
+
+            // Subcategory
+            int? subcId;
+            if (req.SubcategoryId is null) subcId = foundContact.SubcategoryId; // Don't change
+            else subcId = req.SubcategoryId; // Change
+
+            if (categoryName == "Służbowy")
+            {
+                if (subcId is null) return Results.BadRequest("Subcategory is required for category 'Służbowy'.");
+
+                if (custom is not null) return Results.BadRequest("CustomSsubcategory must be empty for category 'Służbowy'.");
+
+                // Subcategory must exist and belong to selected category
+                var subcExists = await db.Subcategories.AnyAsync(el => el.Id == subcId && el.CategoryId == newCategoryId);
+                if (!subcExists) return Results.BadRequest("Invalid subcategory for selected category.");
+
+                foundContact.CategoryId = newCategoryId;
+                foundContact.SubcategoryId = subcId;
+                foundContact.CustomSubcategory = null;
+            }
+            else if (categoryName == "Inny")
+            {
+                if (custom is null)
+                    return Results.BadRequest("Custom ubcategory is required for category 'Inny'.");
+
+                if (subcId is not null)
+                    return Results.BadRequest("Subcategory must be empty for category 'Inny'.");
+
+                foundContact.CategoryId = newCategoryId;
+                foundContact.SubcategoryId = null;
+                foundContact.CustomSubcategory = custom;
+            }
+            else // "Prywatny"
+            {
+                if (subcId is not null)
+                    return Results.BadRequest("SubcategoryIid must be empty for this category.");
+
+                if (custom is not null)
+                    return Results.BadRequest("Custom subcategory must be empty for this category.");
+
+                foundContact.CategoryId = newCategoryId;
+                foundContact.SubcategoryId = null;
+                foundContact.CustomSubcategory = null;
+            }
+        }
+
+        await db.SaveChangesAsync();
+        return Results.Ok("Contact has been edited");
+    }
 }
