665c9abfd8f1ebd14cb9f4fc758c9c4332e22b3d
Lake <jd.jezierskidawid@gmail.com>
2026-02-26 09:56:10 +0100

    User friendly add contact

--- src/frontend/src/views/CreateContact.vue
+++ src/frontend/src/views/CreateContact.vue
@@ -40,6 +40,61 @@ watch(categoryId, () => {
     customSubcategory.value = null;
 });
 
+// User friendly fast validation
+const validEmailMessage = ref("");
+const validPsswdMessage = ref("");
+const validPhoneMessage = ref("");
+
+const touched = ref({
+  email: false,
+  password: false,
+  phoneNumber: false,
+});
+
+function validateEmail () {
+  const check = email.value.trim();
+  if (check == "" || check == null) {
+    validEmailMessage.value = "";
+    return;
+  }
+
+  if (!/^([^@\s]+@[^@\s]+\.[^@\s]+)$/.test(check)) validEmailMessage.value = "Invalid email format";
+}
+
+function validatePswd () {
+  const check = password.value.trim();
+  if (check == "" || check == null) {
+    validPsswdMessage.value = "";
+    return;
+  }
+
+  if (check.length < 8) validPsswdMessage.value = "Password must be at least 8 characters";
+  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$/.test(check)) validPsswdMessage.value = "Invalid password format";
+}
+
+function validatePhone () {
+  if (!phoneNumber.value) {
+    validPhoneMessage.value = "";
+    return;
+  }
+  const check = phoneNumber.value.trim();
+  
+  if (!/^(\+[0-9]{7,15})$/.test(check)) validPhoneMessage.value = "Invalid phone number. Use international format, e.g. +48123456789";
+}
+
+type Field = "email" | "password" | "phoneNumber";
+
+function valdiateField(field: Field) {
+  if (field === "email") validateEmail();
+  if (field === "password") validatePswd();
+  if (field === "phoneNumber") validatePhone();
+}
+// If user goes to another field and last field is not null, blur will activate
+function onBlur(field: Field) {
+  touched.value[field] = true;
+  valdiateField(field);
+}
+
 async function onSubmit() {
     const res = await fetch("/api/contacts", 
     {
@@ -76,10 +131,10 @@ async function onSubmit() {
     <form @submit.prevent="onSubmit" style="display: grid; gap: 10px; max-width: 320px;">
       <input v-model="firstName" placeholder="First name" required />
       <input v-model="lastName" placeholder="Last name" required />
-      <input v-model="email" type="email" placeholder="Email" required />
-      <input v-model="password" type="password" placeholder="Password" required />
+      <input v-model="email" type="email" placeholder="Email" required @blur="onBlur('email')"/>
+      <input v-model="password" type="password" placeholder="Password" required @blur="onBlur('password')"/>
 
-      <input v-model="phoneNumber" placeholder="Phone number (optional)" />
+      <input v-model="phoneNumber" placeholder="Phone number (optional)" @blur="onBlur('phoneNumber')"/>
       <select v-model.number="categoryId" required>
         <option v-for="c in categories" :key="c.id" :value="c.id">
           {{ c.name }}
@@ -99,6 +154,9 @@ async function onSubmit() {
       <button type="submit">Add</button>
 
       <p v-if="message">{{ message }}</p>
+      <p v-if="validEmailMessage" style="color:#ff9494;">{{ validEmailMessage }}</p>
+      <p v-if="validPsswdMessage" style="color:#ff9494;">{{ validPsswdMessage }}</p>
+      <p v-if="validPhoneMessage" style="color:#ff9494;">{{ validPhoneMessage }}</p>
     </form>
   </main>
 </template>
